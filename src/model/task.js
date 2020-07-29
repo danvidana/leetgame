df52fd2f59c79776b8f7d1b53d61799a1deff255
santiagolimasg <santiago@kinedu.com>
2020-07-29 18:58:18 -0500

    minor fix

--- src/model/task.js
+++ src/model/task.js
@@ -5,13 +5,13 @@ const Schema =  mongoose.Schema;
 const GameRecordSchema  = Schema ({
     title: String,
     userId: String,
-    time_played: new Date(),
+  //  time_played: new Date(),
     description: String, 
-    notes: Array<String>,
+  //  notes: Array<String>,
     status: {
         type: Boolean,
         default: false
     }
 });
 
-module.exports = mongoose.model('tasks',TaskSchema);
+module.exports = mongoose.model('tasks',GameRecordSchema);
