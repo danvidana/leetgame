ef7412f069219a49edbebcb35b6bb1760681788c
santiagolimasg <santiago@kinedu.com>
2020-07-31 20:13:16 -0500

    added routes for login and signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -46,4 +46,50 @@ router.get('/delete/:id',  async (req,res) =>{
     res.redirect('/');
 })
 
+//login and signup
+router.get('/register', (req, res) => {
+    res.render('register', { });
+});
+
+router.post('/register', (req, res, next) => {
+    Account.register(new Account({ username : req.body.username }), req.body.password, (err, account) => {
+        if (err) {
+          return res.render('register', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, () => {
+            req.session.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+
+router.get('/login', (req, res) => {
+    res.render('login', { user : req.user, error : req.flash('error')});
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res, next) => {
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
